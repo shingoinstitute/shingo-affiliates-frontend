6add83acea6d3a280510031220131beab3d77612
Angular CLI <angular-cli@angular.io>
2017-08-15 14:27:07 -0600

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ShingoAffiliatesFrontendPage } from './app.po';
+
+describe('shingo-affiliates-frontend App', () => {
+  let page: ShingoAffiliatesFrontendPage;
+
+  beforeEach(() => {
+    page = new ShingoAffiliatesFrontendPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
