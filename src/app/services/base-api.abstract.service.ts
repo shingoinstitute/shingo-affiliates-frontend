3e1e3399160d59088158e0aa066b92084d8053ff
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-08-21 13:25:20 -0600

    Updated API Host to shared server

--- src/app/services/base-api.abstract.service.ts
+++ src/app/services/base-api.abstract.service.ts
@@ -16,7 +16,7 @@ export interface SFSuccessResult {
 }
 
 export abstract class BaseAPIService extends BaseService {
-    protected BaseUrl: string = "http://129.123.47.167";
+    protected BaseUrl: string = "http://144.39.192.15";
     protected BasePort: string = "8080";
 
     protected APIHost() { return `${this.BaseUrl}:${this.BasePort}`; }
