a57f4f4e35fb53ca8c84b5ff2f3ed514a413703c
Craig Blackburn <cr.blackburn89@gmail.com>
2017-08-21 13:08:41 -0600

    admin panel nav tabs

--- src/app/interface/admin-panel/admin-panel.module.ts
+++ src/app/interface/admin-panel/admin-panel.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
+
 import { AdminPanelComponent } from './admin-panel.component';
 import { AdminTabsComponent } from './admin-tabs/admin-tabs.component';
 import { AdminAffiliateTabComponent } from './admin-affiliate-tab/admin-affiliate-tab.component';
@@ -11,7 +14,9 @@ import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 @NgModule({
    imports: [
       CommonModule,
-      AdminPanelRoutingModule
+      AdminPanelRoutingModule,
+      FlexLayoutModule,
+      MaterialModule
    ],
    declarations: [
       AdminPanelComponent,
