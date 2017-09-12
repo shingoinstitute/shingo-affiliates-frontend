0753e5bafcd97ba147d68ea5c015bef458043184
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-09-12 10:01:14 -0600

    Got some U tests done for the affiliate service

--- src/app/services/affiliate/affiliate.service.spec.ts
+++ src/app/services/affiliate/affiliate.service.spec.ts
@@ -6,33 +6,87 @@ import { AffiliateService } from './affiliate.service';
 import { HttpService } from '../http/http.service';
 import { Injector } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 import { Affiliate } from '../../affiliates/affiliate.model';
 import { HttpEvent } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-class HttpServiceMock {
-  public get(): Observable<HttpEvent<any>> {
-    return new Observable<HttpEvent<any>>();
+// TODO: Move MockHttpService to http folder
+// TODO: Override other functions
+// Extend the HttpService so that it can be
+// injected into classes that depend on it.
+// NOTE: The mocked interface needs to match
+// the mockees interface
+class HttpServiceMock extends HttpService {
+
+  private mockAffiliates = [
+    { Id: 'some sf id' },
+    { Id: 'some other sf id' }
+  ];
+
+  constructor() { super(null, null); }
+
+  // As get is used to get arrays of and single 
+  // instances of Affiliates, return the right one
+  public get<T>(url: string): Observable<any> {
+    if (url.includes('some sf id')) return Observable.of(this.mockAffiliates[0]);
+    return Observable.of(this.mockAffiliates);
+  }
+
+  // Post always returns an SFSuccessResponse for Affiliates
+  public post<T>(url: string, body: any): Observable<any> {
+    return Observable.of({ success: true, id: 'some sf id', errors: [] });
   }
 }
 
 describe('AffiliateService', () => {
+
+  // No need to use the Angular TestBed
+  // as we simply can inject the mock 
+  // ourselves (we don't care about it Singletonness)
+  let service: AffiliateService;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        AffiliateService,
-        HttpClientTestingModule,
-        { provide: HttpService, useClass: HttpServiceMock }
-      ]
+    service = new AffiliateService(new HttpServiceMock());
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+    expect(service.getAll).not.toBeUndefined();
+    expect(service.getById).not.toBeUndefined();
+    expect(service.create).not.toBeUndefined();
+    expect(service.update).not.toBeUndefined();
+    expect(service.delete).not.toBeUndefined();
+    expect(service.search).not.toBeUndefined();
+    expect(service.searchCMS).not.toBeUndefined();
+    expect(service.describe).not.toBeUndefined();
+    expect(service.map).not.toBeUndefined();
+  });
+
+  // We should get the "mock" affiliates back
+  it('should get all Affiliates', () => {
+    service.getAll().subscribe(res => {
+      expect(res.length).toBe(2);
+      expect(res[0]).not.toBeUndefined();
+      expect(res[0].sfId).toBe('some sf id');
+      expect(res[1].sfId).toBe('some other sf id');
     });
   });
 
-  it('should be created', inject([AffiliateService], (service: AffiliateService) => {
-    expect(service).toBeTruthy();
-  }));
+  // We should get back the first "mock" affiliate
+  it('should get an affiliate by id', () => {
+    service.getById('some sf id').subscribe(res => {
+      expect(res).not.toBeUndefined();
+      expect(res.sfId).toBe('some sf id');
+    });
+  });
 
-  it('should get all Affiliates', inject([AffiliateService, HttpTestingController], (service: AffiliateService, httpMock: HttpTestingController) => {
-    service.getAll().subscribe(res => expect(true).toBe(true));
-  }));
+  // We should be able to create some affiliate
+  it('should create an affiliate', () => {
+    const test = new Affiliate({ Name: 'test aff' });
+    service.create(test).subscribe(res => {
+      expect(res).not.toBeUndefined();
+      expect(res.id).toBe('some sf id');
+    });
+  });
 
 });
