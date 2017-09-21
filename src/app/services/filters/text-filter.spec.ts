76a51b43846b3e5b521259c41134a2ebf82791f0
Craig Blackburn <cr.blackburn89@gmail.com>
2017-09-21 14:24:25 -0600

    more unit tests

---
+++ src/app/services/filters/text-filter.spec.ts
@@ -0,0 +1,34 @@
+import { TextFilter } from './text-filter';
+
+describe('TextFiler', () => {
+
+  let service: TextFilter<any>;
+  beforeEach(() => {
+    service = new TextFilter('filter name');
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('expects an array to be filtered by a string', () => {
+    // inform service of new filter 'keyword'
+    service.dataChange.next('filter name');
+
+    const data = [
+      'filter name', 
+      'some other filter name',
+      'name filter',
+      'foo',
+      'bar', 
+      'baz'
+    ];
+
+    const fData = service.applyFilter(data);
+
+    expect(fData.length).toEqual(2);
+    expect(fData[0]).toEqual('filter name');
+    expect(fData[1]).toEqual('some other filter name');
+  });
+
+});
---
+++ src/app/services/router/router.service.spec.ts
@@ -0,0 +1,21 @@
+import { RouterService } from './router.service';
+import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
+describe('RouterService', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        RouterService,
+        RouterTestingModule.withRoutes([])
+      ]
+    });
+  });
+
+  // it('should be created', () => {
+    
+  // });
+
+});
