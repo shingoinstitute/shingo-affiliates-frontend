23cf52430caabf88909eee1de82c9989849dfcf7
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-10-19 10:36:33 -0600

    Added generic text response dialog

---
+++ src/app/shared/components/text-response-dialog/text-response-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { MD_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-text-response-dialog',
+  templateUrl: './text-response-dialog.component.html'
+})
+export class TextResponseDialogComponent {
+
+  public title: string;
+  public message: string;
+  public acceptText: string;
+  public cancelText: string;
+  public reason: string;
+
+  constructor( @Inject(MD_DIALOG_DATA) public data: { title: string, message: string, acceptText: string, cancelText: string }) {
+    this.title = data.title;
+    this.message = data.message;
+    this.acceptText = data.acceptText;
+    this.cancelText = data.cancelText;
+  }
+}
