f3b7071ab8bbd5bd2a4c5bba79b0ab975465f120
Craig Blackburn <cr.blackburn89@gmail.com>
2017-11-15 16:11:28 -0700

    oh boi, lots of bug fixes

--- src/app/services/facilitator/facilitator-data-source.service.ts
+++ src/app/services/facilitator/facilitator-data-source.service.ts
@@ -21,17 +21,26 @@ export class FacilitatorDataSource extends APIDataSource<FacilitatorService, Fac
 
     if (!this.sort.active || this.sort.direction === '') { return data; }
 
-    return data.sort((a, b) => {
+    return data.sort((a: Facilitator, b: Facilitator) => {
       let propA: number | string = '';
       let propB: number | string = '';
 
+      console.log('SORT BY: ' + this.sort.active);
+
       switch (this.sort.active) {
-        case 'name': [propA, propB] = [a.name, b.name]; break;
-        default: [propA, propB] = [a.name, b.name];
+        case 'name': [propA, propB] = [a.lastName, b.lastName]; break;
+        case 'email': [propA, propB] = [a.email, b.email]; break;
+        default: [propA, propB] = [a.lastName, b.lastName];
       }
 
-      const valueA = isNaN(+propA) ? propA : +propA;
-      const valueB = isNaN(+propB) ? propB : +propB;
+      let valueA = isNaN(+propA) ? propA : +propA;
+      let valueB = isNaN(+propB) ? propB : +propB;
+
+      if (typeof valueA === 'string')
+        valueA = valueA.toLowerCase();
+
+      if (typeof valueB === 'string')
+        valueB = valueB.toLowerCase();
 
       return (valueA < valueB ? -1 : 1) * (this.sort.direction === 'asc' ? 1 : -1);
     });
