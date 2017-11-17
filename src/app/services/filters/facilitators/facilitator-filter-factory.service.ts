149503cc4f57f900289931c7909a8d7f4c811457
Craig Blackburn <cr.blackburn89@gmail.com>
2017-11-17 14:19:13 -0700

    added name filter to facilitator data table

---
+++ src/app/services/filters/facilitators/facilitator-filter-factory.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { FilterFactory } from '../filter-factory.abstract.service';
+import { TextFilter } from '../text-filter';
+import { Facilitator } from '../../../facilitators/facilitator.model';
+
+
+@Injectable()
+export class FacilitatorFilterFactory {
+
+  public static id: number = 0;
+
+  public createTextFilter(name: string = `FacilitatorFilterFactory:${FacilitatorFilterFactory.id}`): TextFilter<Facilitator> {
+    FacilitatorFilterFactory.id++;
+    return new TextFilter<Facilitator>(name);
+  }
+
+}
