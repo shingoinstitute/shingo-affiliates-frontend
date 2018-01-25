e2378529030c1ba0ff9e9e6611e708db5f5f8104
Abraham White <abelincoln.white@gmail.com>
2018-01-25 11:45:12 -0700

    Add webinar announcement

--- src/app/services/announcement/announcement.service.ts
+++ src/app/services/announcement/announcement.service.ts
@@ -15,6 +15,11 @@ export class AnnouncementService {
     return Observable.create(observer => {
       observer.next([
         {
+          'title': 'Webinar Video',
+          'message': 'If you missed the webinar, be sure to <a href="https://youtu.be/tlKbdAtQIGk">watch it here.</a>',
+          'priority': 5
+        },
+        {
           'title': 'Registration URL Update',
           'message': 'We have updated the system to not require a registration URL if the workshop is public.',
           'priority': 0
