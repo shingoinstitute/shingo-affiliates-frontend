2292f48ea6831c3e761d401a566ebdef8725a663
Abraham White <abelincoln.white@gmail.com>
2018-03-06 12:53:51 -0700

    Implement some UX bug fixes
    Adds some user interface bug fixes:
    1. Fixes confirm and text dialog button ordering
    2. Makes confirm and cancel text clearer when deleting or canceling
       a workshop
    3. Fixes a bug where a workshop would not be removed from the data
       table when deleted

--- src/app/shared/components/text-response-dialog/text-response-dialog.component.ts
+++ src/app/shared/components/text-response-dialog/text-response-dialog.component.ts
@@ -1,6 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 
+// tslint:disable-next-line:interface-name
+export interface TextResponseData {
+  title?: string;
+  message?: string;
+  acceptText?: string;
+  cancelText?: string;
+  destructive?: boolean;
+}
+
 @Component({
   selector: 'app-text-response-dialog',
   templateUrl: './text-response-dialog.component.html'
@@ -12,11 +21,13 @@ export class TextResponseDialogComponent {
   public acceptText: string;
   public cancelText: string;
   public reason: string;
+  public destructive: boolean;
 
-  constructor( @Inject(MAT_DIALOG_DATA) public data: { title: string, message: string, acceptText: string, cancelText: string }) {
-    this.title = data.title;
-    this.message = data.message;
-    this.acceptText = data.acceptText;
-    this.cancelText = data.cancelText;
+  constructor( @Inject(MAT_DIALOG_DATA) public data: TextResponseData) {
+    this.title = data.title ? data.title : '';
+    this.message = data.message ? data.message : '';
+    this.acceptText = data.acceptText ? data.acceptText : '';
+    this.cancelText = data.cancelText ? data.cancelText : '';
+    this.destructive = typeof data.destructive !== 'undefined' ? data.destructive : false;
   }
 }
