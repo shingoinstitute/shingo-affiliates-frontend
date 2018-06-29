5c570992d92e3a846f6d4e3a110e28b7f6b7da0a
Abraham White <abelincoln.white@gmail.com>
2018-06-29 10:58:20 -0600

    Update announcements

--- src/app/services/announcement/announcement.service.ts
+++ src/app/services/announcement/announcement.service.ts
@@ -14,14 +14,9 @@ export class AnnouncementService {
     /** TODO: Remove after actual API route is implemented */
     return new Observable<Announcement[]>(observer => {
       const announcements: Announcement[] = [
-        new Announcement('Webinar - April 30, 2018',
-          // tslint:disable-next-line:max-line-length
-          'If you missed the webinar, be sure to <a href="https://youtu.be/U_FxdMXDOic">watch it here.</a> When you\'re done, make sure to fill out the survey in the video\'s description.' ,
-          5
-        ),
         new Announcement('Webinar - July 17, 2018',
           // tslint:disable-next-line:max-line-length
-          'The next affiliate webinar is on July 17 at 9 am MT. Please visit <a href="https://connect.usu.edu/shingo">https://connect.usu.edu/shingo</a> to join us live, or watch the recording that will be posted here a couple of days later. Remember that all facilitators are required to view the webinars, so don’t forget to log back into the portal. See you soon!',
+          'The next affiliate webinar is on July 17 at 9 am MT. Please visit <a href="https://connect.usu.edu/shingo">https://connect.usu.edu/shingo</a> to join us live, or watch the recording that will be posted here a couple of days later. Remember that all facilitators are required to view the webinars, so don’t forget to log back into the portal. See you soon!<br/><br/>If you missed the April 30 webinar, be sure to <a href="https://youtu.be/U_FxdMXDOic">watch it here.</a> When you\'re done, make sure to fill out the survey in the video\'s description.',
           4
         )
       ];
