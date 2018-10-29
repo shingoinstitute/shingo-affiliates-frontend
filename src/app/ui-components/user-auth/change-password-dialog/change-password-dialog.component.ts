632821957cd6ee7ea7be85d939a1af9bb5511858
Abraham White <abelincoln.white@gmail.com>
2018-10-29 14:33:49 -0600

    Move change-password-dialog to reactiveForm

--- src/app/ui-components/user-auth/change-password-dialog/change-password-dialog.component.ts
+++ src/app/ui-components/user-auth/change-password-dialog/change-password-dialog.component.ts
@@ -2,6 +2,42 @@ import { Component } from '@angular/core'
 import { MatDialogRef } from '@angular/material'
 
 import { AuthService } from '../../../services/auth/auth.service'
+import { FormGroup, FormControl, Validators, ValidatorFn } from '@angular/forms'
+import { tuple } from '../../../util/functional'
+
+export const mustMatchValidator = (
+  names: string[],
+  comparator: (a: unknown, b: unknown) => boolean = (a, b) => a === b,
+): ValidatorFn => control => {
+  if (!control.parent) return null
+
+  if (names.length === 0)
+    throw new Error(
+      'mustMatchValidator(): must provide at least one control name',
+    )
+
+  const values = names.map(n => {
+    const g = control.parent.get(n)
+    if (!g)
+      throw new Error(
+        `mustMatchValidator(): control ${n} does not exist in parent group`,
+      )
+    g.valueChanges.subscribe(() => control.updateValueAndValidity())
+    return tuple(n, g.value)
+  })
+
+  const value = control.value
+  let reason = null
+  const allEqual = values.every(([n, a]) => {
+    if (!comparator(a, value)) {
+      reason = `Control ${n} with value ${a} does not match ${value}`
+      return false
+    }
+    return true
+  })
+
+  return allEqual ? null : { must_match: reason }
+}
 
 @Component({
   selector: 'app-change-password-dialog',
@@ -10,10 +46,15 @@ import { AuthService } from '../../../services/auth/auth.service'
 })
 // tslint:disable-next-line:component-class-suffix
 export class ChangePasswordDialog {
-  public password!: string
-  public newPassword!: string
-  public confirmPassword!: string
   public isLoading = false
+  public pwForm = new FormGroup({
+    password: new FormControl('', Validators.required),
+    newPassword: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      mustMatchValidator(['newPassword']),
+    ]),
+  })
 
   constructor(
     public dialogRef: MatDialogRef<ChangePasswordDialog>,
@@ -21,16 +62,30 @@ export class ChangePasswordDialog {
   ) {}
 
   public onSubmit() {
-    if (this.newPassword !== this.confirmPassword) return
+    if (!this.pwForm.valid) return
+    const { password, newPassword } = this.pwForm.value as {
+      password: string
+      newPassword: string
+      confirmPassword: string
+    }
     if (!this.auth.user) return
     this.isLoading = true
-    this.auth
-      .login({ email: this.auth.user.email, password: this.password })
-      .subscribe(data => {
-        this.auth.changeUserPassword(this.newPassword).subscribe(() => {
+    this.auth.login({ email: this.auth.user.email, password }).subscribe(
+      () => {
+        this.auth.changeUserPassword(newPassword).subscribe(() => {
           this.isLoading = false
           this.dialogRef.close('Password Changed!')
         })
-      })
+      },
+      err => {
+        this.isLoading = false
+        console.error('GOT ERROR', err)
+        if (err.error.error.error === 'INVALID_PASSWORD') {
+          this.pwForm.controls['password'].setErrors({ invalidPassword: true })
+        } else {
+          throw err
+        }
+      },
+    )
   }
 }
