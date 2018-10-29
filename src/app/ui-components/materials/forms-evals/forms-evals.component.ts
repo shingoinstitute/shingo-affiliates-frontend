4df4052d803625cd7a2a0da958ccf3efa9da7c27
Abraham White <abelincoln.white@gmail.com>
2018-10-29 11:47:26 -0600

    Finish converting forms-evals dialog

--- src/app/ui-components/materials/forms-evals/forms-evals.component.ts
+++ src/app/ui-components/materials/forms-evals/forms-evals.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core'
+import { Observable, of } from 'rxjs'
+import {
+  FileSystemNode,
+  folderNode,
+  fileNode,
+} from '../../file-tree/file-tree.component'
 
 @Component({
   selector: 'app-forms-evals',
@@ -7,4 +13,32 @@ import { Component } from '@angular/core'
 })
 export class FormsEvalsComponent {
   public folder = { Evaluation: false }
+  TREE_DATA: Observable<FileSystemNode[]> = of([
+    folderNode(
+      'Instructor Evaluation',
+      [
+        fileNode(
+          'Online',
+          'https://usu.co1.qualtrics.com/jfe/form/SV_9G2dNDlLoJC8SUZ',
+        ),
+        fileNode(
+          'English',
+          'https://usu.box.com/s/hqg8su339i5cvuqnnov6z2xt0jyb7o8r',
+        ),
+        fileNode(
+          'Español',
+          'https://usu.box.com/s/y0mcnq3sw5lr7j6xun2g7jh998e88j3n',
+        ),
+      ],
+      'https://usu.app.box.com/files/0/f/8549280365/Instructor_Evaluations',
+    ),
+    fileNode(
+      'Attendance Form',
+      'https://usu.box.com/s/9wj1sig0y1ivi23s05pfbdu66ozsjw82',
+    ),
+    fileNode(
+      'Name Sign',
+      'https://usu.box.com/s/99cio2jdoqk17hgo6cqm9q6v3j39d3mv',
+    ),
+  ])
 }
